5b838b31619f6c9b7564054ddbbbea7bbfadd1d7
Kiruthiga <kiruthigavelusamy2005@gmail.com>
2021-02-26 07:01:31 +0530

    second line addded

--- HelloWorldDevOps/src/DevOpsDemo.java
+++ HelloWorldDevOps/src/DevOpsDemo.java
@@ -3,7 +3,9 @@ public class DevOpsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World DevOps");
+		
+		System.out.println("One more line added");
 
 	}
 
